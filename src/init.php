e7f42dc566a844424de69ed131e4b001225526b8
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-01-06 02:06:43 +0900

    fix code styles

--- src/init.php
+++ src/init.php
@@ -5,22 +5,28 @@
  * @package Code_Editor_Blocks
  */
 
+namespace Code_Editor_Blocks;
+
 add_action(
 	'enqueue_block_editor_assets',
 	function () {
-		$deps = [
-			'wp-blocks',
-			'wp-components',
-			'wp-data',
-			'wp-element',
-			'wp-editor',
-			'wp-edit-post',
-			'wp-i18n',
-			'code-editor'
-		];
-		wp_enqueue_script( 'code-editor-blocks', plugins_url( 'dist/main.js', dirname( __FILE__ ) ), $deps, 1, true );
 		wp_enqueue_code_editor( [ 'type' => 'text/html' ] );
 		wp_add_inline_style( 'code-editor', '.CodeMirror { font-size: 14px }' );
+		wp_enqueue_script(
+			'code-editor-blocks',
+			plugins_url( 'dist/main.js', PLUGIN_FILE ),
+			[
+				'wp-blocks',
+				'wp-components',
+				'wp-data',
+				'wp-element',
+				'wp-editor',
+				'wp-edit-post',
+				'wp-i18n',
+				'code-editor',
+			],
+			get_plugin_data()['Version'],
+			true
+		);
 	}
 );
-
